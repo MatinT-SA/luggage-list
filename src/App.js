0de87560744b79fcf5acb92dceda65904dcca27b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-20 20:05:32 +0330

    stats section

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -125,11 +125,28 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>
+          No items yet? Let’s gear up and get that packing list ready! 🎒🚀
+        </em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
       <em>
-        You have Y items in your list. You have already packed Y (Y%) items.
+        {percentage === 100
+          ? "Packing perfection! Now go make some memories. 📸✈️"
+          : `You have ${numItems} items in your list. You have already packed{" "}
+        ${numPacked} (${percentage}%) items.`}
       </em>
     </footer>
   );
