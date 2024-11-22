f4689368d52679cdbbc92a368137772e71fef4f7
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-11-22 23:08:22 +0330

    responsiveness for form and consolidating styles in general for smaller than 1000px laptop

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,26 +20,28 @@ export default function Form({ onAddItem }) {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>List all the things you need for your trip</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        required
-      />
-      <button className="add-form__button">
-        <span>Add</span>
-      </button>
+      <div className="add-form__row">
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <button className="add-form__button">
+          <span>Add</span>
+        </button>
+      </div>
     </form>
   );
 }
